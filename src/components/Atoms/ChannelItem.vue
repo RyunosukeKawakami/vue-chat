af995901fb74ac932000bc544968b664b227283e
RyunosukeKawakami <k.r.doragon28@gmail.com>
2020-02-17 07:24:08 +0900

    チャンネルの雛形を作成

---
+++ src/components/Atoms/ChannelItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <a href="#" class="list-group-item list-group-item-action">Channel</a> 
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Templates/Home.vue
+++ src/components/Templates/Home.vue
@@ -1,16 +1,22 @@
 <template>
-    <header>
-        <navigation></navigation>
-    </header>
+    <div>
+        <header>
+            <navigation></navigation>
+        </header>
+        
+        <channel class="col-3"></channel>
+    </div>
 </template>
 
 <script>
-import Navigation from "../Organisms/Navigation.vue"
+import Navigation from "@/components/Organisms/Navigation.vue"
+import Channel from "@/components/Organisms/Channel.vue"
 
 export default {
     name: "Home",
     components:{
-        Navigation
+        Navigation,
+        Channel
     }
 }
 </script>
