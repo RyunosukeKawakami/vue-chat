3013ee7a7f917bc678906ea2b874a7d7721d64f6
RyunosukeKawakami <k.r.doragon28@gmail.com>
2020-02-16 20:24:04 +0900

    コンポーネントを抽出

---
+++ src/components/Templates/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <navigation></navigation>
+    </header>
+</template>
+
+<script>
+import Navigation from "../Organisms/Navigation.vue"
+
+export default {
+    name: "Home",
+    components:{
+        Navigation
+    }
+}
+</script>
+
+<style>
+
+</style>
