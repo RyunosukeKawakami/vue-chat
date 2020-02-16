d78643526f9f80d6c4eb58c5521021cdafa8bb6d
RyunosukeKawakami <k.r.doragon28@gmail.com>
2020-02-16 20:24:53 +0900

    ナビゲーションバーの雛形を作成

---
+++ src/components/Atoms/NavItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li class="nav-link" :class="activity" href="#">{{name}}</li>
+</template>
+
+<script>
+export default {
+  props:[
+    "name",
+    "activity"
+  ]
+
+}
+</script>
---
+++ src/components/Organisms/Navigation.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar navbar-light" style="background-color: #73add3">
+        <a class="navbar-brand" href="#">vue-chat</a>
+        <ul class="nav nav-pills">
+            <nav-item v-for="NavItemObj of NavItemObjList" :key="NavItemObj.id" :name="NavItemObj.name" :activity="NavItemObj.activity"></nav-item>
+        </ul>
+    </nav>
+</template>
+
+<script>
+import NavItem from "../Atoms/NavItem.vue"
+
+class NavItemObj{
+    constructor(id, name, activity){
+        this.id = id
+        this.name = name;
+        this.activity = activity;
+    }    
+}
+
+const NavItemObjList = [
+    new NavItemObj(1, "Home", "active"),
+    new NavItemObj(2, "Login", ""),
+    new NavItemObj(3, "Logout", ""),
+]
+
+export default {
+    data(){
+        return{
+            NavItemObjList
+        }
+    },
+
+    components:{
+        NavItem
+    }
+}
+</script>
