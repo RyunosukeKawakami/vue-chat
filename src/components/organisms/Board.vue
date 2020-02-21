66fbdcb8fa4d1960d34385502fe542aa61a48e5f
RyunosukeKawakami <kawadora28@gmail.com>
2020-02-21 22:27:50 +0900

    各項目の見出しを追加

--- src/components/organisms/Board.vue
+++ src/components/organisms/Board.vue
@@ -1,15 +1,20 @@
 <template>
+<div>
+    <organism-title>Message Board</organism-title>
     <div class="overflow-auto" style="max-height: 650px">
         <board-card v-for="i in 20" :key="i"></board-card>
     </div>
+</div>
 </template>
 
 <script>
 import BoardCard from "@/components/atoms/BoardCard.vue"
+import OrganismTitle from "@/components/atoms/OrganismTitle.vue"
 
 export default {
     components:{
-        BoardCard
+        BoardCard,
+        OrganismTitle
     }
 }
 </script>
