ba5faba79fed554eb494d3eee3b6ec41de463cd6
RyunosukeKawakami <kawadora28@gmail.com>
2020-02-21 23:10:33 +0900

    Memberの雛形を完成

---
+++ src/components/organisms/Member.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <organism-title>Member</organism-title>
+    <member-item></member-item>
+</div>
+</template>
+
+<script>
+import OrganismTitle from "@/components/atoms/OrganismTitle.vue"
+import MemberItem from "@/components/atoms/MemberItem.vue"
+
+export default {
+    components:{
+        OrganismTitle,
+        MemberItem
+    }
+}
+</script>
+
+<style>
+
+</style>
