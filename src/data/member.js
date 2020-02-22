d72480a36ceffdda678139a2604321065e2b98d5
RyunosukeKawakami <kawadora28@gmail.com>
2020-02-22 16:28:28 +0900

    DBの代わりに情報を保持するクラスを追加

---
+++ src/data/member.js
@@ -0,0 +1,8 @@
+export default class member{
+    constructor(id, password, name, is_online) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.is_online = is_online;
+    }
+}
